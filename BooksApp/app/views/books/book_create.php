530f67eafc161b346bb30600fd9dbb1ffbe171ee
Tomáš <tomas.havel@tul.cz>
2026-03-19 15:29:34 +0100

    #3 Ukol 05 formular

--- BooksApp/app/views/books/book_create.php
+++ BooksApp/app/views/books/book_create.php
@@ -21,8 +21,11 @@
                         <input type="text" id="title" name="title" required>
                     </div>                    
                     <div>
-                        <label for="author">Autor <span>*</span></label>
-                        <input type="text" id="author" name="title" required>
+                        <label for="author">Autor knihy<span>*</span></label>
+                        <input type="text" id="author" name="title" required placeholder="Prijmeni, jmeno">
+                    </div>
+                        <label for="isbn">ISBN <span>*</span></label>
+                        <input type="text" id="isbn" name="isbn" >
                     </div>
                      <div>
                         <label for="category">Kategorie</label>
@@ -36,8 +39,42 @@
                         <label for="year">Rok vydani<span>*</span></label>
                         <input type="number" id="year" name="year" required>
                     </div>                   
-                </div>
+                    <div>
+                        <label for="price">Cena<span></span></label>
+                        <input type="number" id="price" name="price" step="0.5">
+                    </div>                   
+
+                    <div>
+                        <label for="link">Odkaz <span></span></label>
+                        <input type="text" id="link" name="link" >
+                    </div>
+                    <div>
+                        <label for="description">Popis knihy <span></span></label>
+                        <textarea id="description" name="description" rows="10" cols="50">Popis knihy</textarea>
+                    </div>
+                    <div>
+                        <label >Obrázky (můžete nahrát více)</label>
+                        <label>
+                            <span>Klikni pro výběr souborů</span>
+                            <span>JPG / PNG / WebP – více souborů najednou</span>
+                            <input type="file" id="images" name="images[]" multiple accept="image/*" class="hidden">
+                        </label>
+                    </div>
                 
+
+                    
+
+
+
+
+
+
+
+
+
+
+                </div>
+
 
 
 
