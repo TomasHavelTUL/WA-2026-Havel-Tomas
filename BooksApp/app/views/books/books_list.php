9a9633ec528e92736a7d1090eacec9abf51dbb2b
Tomáš <tomas.havel@tul.cz>
2026-03-26 15:27:39 +0100

    #4 Úkol 08 - Routování

---
+++ BooksApp/app/views/books/books_list.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Knihovna - Seznam knih</title>
+</head>
+<body>
+    <header>
+        <h1>Aplikace Knihovna</h1>
+
+
+        <nav>
+            <ul>
+                <li><a href="/WA-2026-Havel-Tomas/BooksApp/public/index.php">Seznam knih (Domu)</a></li>
+                <li><a href="/WA-2026-Havel-Tomas/BooksApp/public/index.php?url=book/create">Pridat novou knihu</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Dostupne knihy</h2>
+
+        <p>Zde se brzy objevi seznam knih nacteny z dabaze.</p>
+    </main>
+
+    <footer>
+        <p>&copy; WA 2026 - Vyukovy projekt</p>
+    </footer>
+</body>
+</html>
